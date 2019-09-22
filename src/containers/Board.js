f76c6bafe9b59072e365ebfde6dec152895609be
storiaca <storiaca@gmail.com>
2019-09-22 14:41:09 +0200

    setup init state

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -3,10 +3,17 @@ import GameStats from "../components/GameStats";
 import Square from "../components/Square";
 import classes from "./Board.module.css";
 class Board extends Component {
+  state = {
+    timer: 0,
+    clicked: 0,
+    lives: 0,
+    level: 1
+  };
+
   render() {
     const n = 100;
     const squareBoard = [...Array(n)].map((e, i) => (
-      <Square key={i} number={i + 1} />
+      <Square key={i} number={i} />
     ));
 
     return (
